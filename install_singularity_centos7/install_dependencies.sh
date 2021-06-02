d239073057801023e2f9d7869363c3b2bf6c3cdb
Cloud User <dani@dockervm.2iii2tfzjrxupafcif4zdsmeqf.ax.internal.cloudapp.net>
2021-06-02 11:33:20 +0000

    add install_dependencies.sh file

---
+++ install_singularity_centos7/install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo yum update -y && \
+     sudo yum groupinstall -y 'Development Tools' && \
+     sudo yum install -y \
+     openssl-devel \
+     libuuid-devel \
+     libseccomp-devel \
+     wget \
+     squashfs-tools \
+     cryptsetup
