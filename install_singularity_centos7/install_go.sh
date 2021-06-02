ea59db29147877e89cc329d10c6d8dd6b26ecafd
Cloud User <dani@dockervm.2iii2tfzjrxupafcif4zdsmeqf.ax.internal.cloudapp.net>
2021-06-02 12:33:24 +0000

    add install_go.sh file

---
+++ install_singularity_centos7/install_go.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# install go version 1.16.4
+# https://sylabs.io/guides/3.5/admin-guide/installation.html
+
+export VERSION=1.16.4 OS=linux ARCH=amd64 && \
+    wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+    sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+    rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
+    echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
+    source ~/.bashrc
